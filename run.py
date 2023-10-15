422ef0cd152b47b4b28ead9095ac06fc4e386669
soulZhaoL <373088079@qq.com>
2023-10-15 14:35:08 +0800

    test

--- run.py
+++ run.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
-    parser.add_argument('--use_multi_gpu', type=bool, action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
     parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
 
     # de-stationary projector params
