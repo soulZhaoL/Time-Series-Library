e55b68de2f55478bd0bdec04760e3469bff5002d
soulZhaoL <373088079@qq.com>
2023-10-28 12:24:30 +0800

    btc classifycation

--- scripts/classification/TimesNet.sh
+++ scripts/classification/TimesNet.sh
@@ -1,4 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
+root_path=/root/autodl-tmp/dataset
 
 #python -u run.py  --task_name classification  --is_training 1  --root_path /root/autodl-tmp/dataset/EthanolConcentration/  --model_id EthanolConcentration     --model TimesNet  --data UEA  --e_layers 3  --batch_size 16  --d_model 32  --d_ff 32   --top_k 3  --des 'Exp'  --itr 1  --learning_rate 0.001  --train_epochs 30  --patience 10
 
@@ -20,4 +21,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 #
 #python -u run.py  --task_name classification  --is_training 1  --root_path ./dataset/UWaveGestureLibrary/   --model_id UWaveGestureLibrary       --model TimesNet --data UEA  --e_layers 2  --batch_size 16  --d_model 32  --d_ff 64  --top_k 3  --des 'Exp'  --itr 1  --learning_rate 0.001  --train_epochs 30  --patience 10
 
-python -u run.py --task_name classification --is_training 1 --root_path ./dataset/BTC/ --model_id BTC --model TimesNet --data UEA_CSV --e_layers 3 --batch_size 16 --d_model 32 --d_ff 32 --top_k 3 --des 'Exp' --itr 1 --learning_rate 0.001 --train_epochs 30 --patience 10
+python -u run.py --task_name classification --is_training 1 --root_path $root_path/BTC/ --model_id BTC --model TimesNet --data UEA_CSV --e_layers 3 --batch_size 16 --d_model 32 --d_ff 32 --top_k 3 --des 'Exp' --itr 1 --learning_rate 0.001 --train_epochs 30 --patience 10
